defb5fe0abba8e00706667afcc2b31847203ff31
K.Kiyokawa <koichi20110068@gmail.com>
2021-07-28 09:30:35 +0900

    chore: add colmuns to user model

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,7 +3,10 @@ import { PrismaClient } from "@prisma/client"
 export async function seed() {
   const prisma = new PrismaClient()
   prisma.user.createMany({
-    data: [...Array(10).keys()].map((i) => ({ name: `user${i}` })),
+    data: Array.from(Array(10).keys()).map((i) => ({
+      name: `user${i}`,
+      email: `user${i}@example.com`,
+    })),
   })
   await prisma.$disconnect()
 }
