8041a7f0dedcf7af9b848ba9b74a28c33a2c9f26
K.Kiyokawa <koichi20110068@gmail.com>
2021-08-01 11:06:06 +0900

    feat: add post model

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
+import "reflect-metadata"
 import fastify from "fastify"
-import { setupRoute } from "./router"
+// import { setupRoute } from "./router"
+import mercurius from "mercurius"
+import { resolvers } from "@generated/type-graphql"
+import { buildSchema } from "type-graphql"
+import { prisma } from "./modules/prisma"
 
-const isProd = process.env.NODE_ENV === "production"
-const app = fastify({ logger: { prettyPrint: isProd } })
+async function main() {
+  const isProd = process.env.NODE_ENV === "production"
+  const app = fastify({ logger: { prettyPrint: isProd } })
+  app.register(mercurius, {
+    schema: await buildSchema({
+      resolvers,
+    }),
+    graphiql: true,
+    context: () => ({ prisma }),
+  })
 
-setupRoute(app)
+  // setupRoute(app)
 
-app.listen(process.env.PORT ?? 8080)
+  app.listen(process.env.PORT ?? 8080)
+}
+
+main()
