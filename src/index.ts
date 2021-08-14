e03f254b44fc24d883e6773761e493a730c57303
K.Kiyokawa <koichi20110068@gmail.com>
2021-08-14 19:14:10 +0900

    chore

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import "reflect-metadata"
 import fastify from "fastify"
 import mercurius from "mercurius"
+import cookie from "fastify-cookie"
+import session from "@fastify/session"
+import MongoStore from "connect-mongo"
+import dotenv from "dotenv"
 import { resolvers } from "@generated/type-graphql"
 import { buildSchema } from "type-graphql"
 import { prisma } from "./modules/prisma"
@@ -8,9 +12,16 @@ import { setupRoute } from "./router"
 import { setupSwagger } from "./modules/swagger"
 
 async function main() {
+  dotenv.config()
   const isProd = process.env.NODE_ENV === "production"
-  const app = fastify({ logger: { prettyPrint: isProd } })
+  console.log(process.env)
+  const app = fastify({ logger: { prettyPrint: !isProd } })
 
+  app.register(cookie)
+  app.register(session, {
+    secret: "ehgaoignanowngonaognlewigoheaoigoojj",
+    store: MongoStore.create({ mongoUrl: process.env.DATABASE_URL }),
+  })
   app.register(mercurius, {
     schema: await buildSchema({
       resolvers,
@@ -25,4 +36,4 @@ async function main() {
   app.listen(process.env.PORT ?? 8080)
 }
 
-main()
+main().catch(console.error)
